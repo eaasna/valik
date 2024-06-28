4e2b4346070d519acf6cffa5fedfa9d166929c4a
Evelin Aasna <evelin.aasna@fu-berlin.de>
2024-06-28 12:06:04 +0200

    Alphabet adapter boilerplate

--- include/valik/search/query_record.hpp
+++ include/valik/search/query_record.hpp
@@ -5,7 +5,7 @@
 #include <sstream>
 
 #include <valik/split/metadata.hpp>
-#include <utilities/hashable_Dna.hpp>
+#include <utilities/alphabet_wrapper/matcher/stellar_matcher.hpp>
 
 #include <seqan3/alphabet/nucleotide/dna4.hpp>
 #include <seqan/sequence.h>
